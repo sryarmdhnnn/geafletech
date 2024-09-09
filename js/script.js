8583850f55a5ff60cd0200b7a90552b754275f71
sryarmdhnnn <sryarmdhnn@gmail.com>
2024-09-09 14:43:52 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//Ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar hamburger menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Menu
+const modalViews = document.querySelectorAll(".experience__modal"),
+  modalBtns = document.querySelectorAll(".experience__button"),
+  modalClose = document.querySelectorAll(".experience__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
