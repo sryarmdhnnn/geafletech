e21064c3d947720df3ba05ccc8842eb81b615dfd
sryarmdhnnn <sryarmdhnn@gmail.com>
2024-09-12 23:29:20 +0700

    eagfgdhvsbn

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-// Toogle class active
+// Toogle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 //Ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// Klik di luar hamburger menu
-const hamburger = document.querySelector("#hamburger-menu");
+// Toogle class active search
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+//Ketika hamburger menu di klik
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-btn");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
 
